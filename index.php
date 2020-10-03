9f8aeaced90228abe55a4a9a4f951ebcff813468
Sumanpal3108 <64361664+Sumanpal3108@users.noreply.github.com>
2020-10-03 13:12:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 	<link href="../assets/dist/css/bootstrap.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 
-	<!--Bootstrap scripts 1-->
+	<!--Bootstrap scripts1-->
 	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
